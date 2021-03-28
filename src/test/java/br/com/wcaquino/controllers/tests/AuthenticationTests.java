b3c944b67ada32baa264479fa91b8d69c6acedfb
Thiago Sakurai Paschoal <thiagopaschoal076@gmail.com>
2021-03-27 23:41:26 -0300

    test: add jwt token

--- src/test/java/br/com/wcaquino/controllers/tests/AuthenticationTests.java
+++ src/test/java/br/com/wcaquino/controllers/tests/AuthenticationTests.java
@@ -2,10 +2,14 @@ package br.com.wcaquino.controllers.tests;
 
 import static io.restassured.RestAssured.*;
 
+import io.restassured.http.ContentType;
+import org.apache.groovy.util.Maps;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
 
@@ -43,6 +47,37 @@ public class AuthenticationTests {
                 .statusCode(200)
                 .body("status", Matchers.is("logado"));
 
+    }
+
+    @Test
+    @DisplayName("deve listar todas as contas cadastradas usando JWT como autenticador")
+    public void shouldAuthenticateWithJWT() {
+        final String token = getTokenJwt();
+        given()
+                .header("Authorization", "JWT " + token)
+                .when()
+                .get("http://barrigarest.wcaquino.me/contas")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("$.size()", Matchers.is(3))
+                .body("nome", Matchers.hasItems("Thor", "Captain America", "Iron Man"));
+    }
+
+    private String getTokenJwt() {
+
+        final Map<String, Object> login = Maps.of("email", "thiago@admin.com", "senha", "123456");
+        final String token = given()
+                .body(login)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("http://barrigarest.wcaquino.me/signin")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .path("token");
+        return token;
     }
 
 }
